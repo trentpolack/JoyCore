b93490f219eebb0d1776338e732ef3ade762694b
Trent Polack <trentp@gmail.com>
2026-08-17 13:47:43 -0400

    Honestly can't really remember what changed here.

--- Source/JoyCore/Public/Systems/Rules/SystemicRuleContext.h
+++ Source/JoyCore/Public/Systems/Rules/SystemicRuleContext.h
@@ -9,6 +9,7 @@
 
 /**
  *	FSystemicRuleContext Structure Definition.
+ *		Used to accumulate context and information/results while processing a rule. Not intended for Blueprint usage.
  */
 USTRUCT(BlueprintType, Category="Game|Systems")
 struct JOYCORE_API FSystemicRuleContext
@@ -16,21 +17,21 @@ struct JOYCORE_API FSystemicRuleContext
 	GENERATED_BODY()
 	
 	// Initiator of the event.
-	UPROPERTY(BlueprintReadOnly, Transient, VisibleInstanceOnly, AdvancedDisplay, Category = "RuleContext|Transient")
+	UPROPERTY(BlueprintReadOnly, Transient, VisibleInstanceOnly, AdvancedDisplay, Category = "RuleContext")
 	TWeakObjectPtr<AActor> Instigator = nullptr;
 	// Target of the event, if there is one.
-	UPROPERTY(BlueprintReadOnly, Transient, VisibleInstanceOnly, AdvancedDisplay, Category = "RuleContext|Transient")
+	UPROPERTY(BlueprintReadOnly, Transient, VisibleInstanceOnly, AdvancedDisplay, Category = "RuleContext")
 	TWeakObjectPtr<UObject> Target = nullptr;
 	// Source object that caused the event (for general-purpose coverage beyond actors). 
-	UPROPERTY(BlueprintReadOnly, Transient, VisibleInstanceOnly, AdvancedDisplay, Category = "RuleContext|Transient")
+	UPROPERTY(BlueprintReadOnly, Transient, VisibleInstanceOnly, AdvancedDisplay, Category = "RuleContext")
 	TWeakObjectPtr<UObject> Source = nullptr;
 
 	// Cache of named objects throughout the evaluation of a ruleset and reaction. 
-	UPROPERTY(BlueprintReadOnly, Transient, VisibleInstanceOnly, AdvancedDisplay, Category = "RuleContext|Transient")
+	UPROPERTY(BlueprintReadOnly, Transient, VisibleInstanceOnly, AdvancedDisplay, Category = "RuleContext")
 	TMap<FName, UObject*> NamedObjects;
 
 	// Cache of named parameters throughout the evaluation of a ruleset and reaction. 
-	UPROPERTY(BlueprintReadOnly, Transient, VisibleInstanceOnly, AdvancedDisplay, Category = "RuleContext|Transient")
+	UPROPERTY(BlueprintReadOnly, Transient, VisibleInstanceOnly, AdvancedDisplay, Category = "RuleContext")
 	TMap<FName, float> NamedValues;
 
 	/**
